e7865f37c219b660c8d9f3d24db0aa804bdb105d
Adam Treat <treat@kde.org>
2008-02-12 14:43:06 -0500

    * Add action to tabbar to allow closing tabs

--- src/tabwidget.cpp
+++ src/tabwidget.cpp
@@ -1,11 +1,47 @@
 #include "tabwidget.h"
 
 #include <QDebug>
-#include <QTabBar>
+#include <QAction>
+
+TabBar::TabBar(QWidget *parent)
+    : QTabBar(parent)
+{
+    setContextMenuPolicy(Qt::ActionsContextMenu);
+
+    m_actionCloseTab = new QAction(tr("Close Tab"), this);
+    m_actionCloseTab->setVisible(false);
+    connect(m_actionCloseTab, SIGNAL(triggered()), this, SLOT(closeCurrentTab()));
+
+    addAction(m_actionCloseTab);
+}
+
+TabBar::~TabBar()
+{
+}
+
+void TabBar::tabInserted(int index)
+{
+    m_actionCloseTab->setVisible(count() > 1);
+    QTabBar::tabInserted(index);
+}
+
+void TabBar::tabRemoved(int index)
+{
+    m_actionCloseTab->setVisible(count() > 1);
+    QTabBar::tabRemoved(index);
+}
+
+void TabBar::closeCurrentTab()
+{
+    emit closeTab(currentIndex());
+}
 
 TabWidget::TabWidget(QWidget *parent)
     : QTabWidget(parent)
 {
+    TabBar *bar = new TabBar(this);
+    connect(bar, SIGNAL(closeTab(int)), this, SLOT(closeTab(int)));
+    setTabBar(bar);
 }
 
 TabWidget::~TabWidget()
@@ -20,4 +56,13 @@ bool TabWidget::isTabBarVisible() const
 void TabWidget::setTabBarVisible(bool visible)
 {
     tabBar()->setVisible(visible);
+}
+
+void TabWidget::closeTab(int index)
+{
+    if (QWidget *w = widget(index)) {
+        w->deleteLater();
+    }
+
+    removeTab(index);
 }
--- src/tabwidget.h
+++ src/tabwidget.h
@@ -1,7 +1,30 @@
 #ifndef TABWIDGET_H
 #define TABWIDGET_H
 
+#include <QTabBar>
 #include <QTabWidget>
+
+class QAction;
+
+class TabBar : public QTabBar {
+    Q_OBJECT
+public:
+    TabBar(QWidget *parent);
+    ~TabBar();
+
+protected:
+    virtual void tabInserted(int index);
+    virtual void tabRemoved(int index);
+
+Q_SIGNALS:
+    void closeTab(int index);
+
+private Q_SLOTS:
+    void closeCurrentTab();
+
+private:
+    QAction *m_actionCloseTab;
+};
 
 class TabWidget : public QTabWidget {
     Q_OBJECT
@@ -11,6 +34,9 @@ public:
 
     bool isTabBarVisible() const;
     void setTabBarVisible(bool visible);
+
+public Q_SLOTS:
+    void closeTab(int index);
 };
 
 #endif
